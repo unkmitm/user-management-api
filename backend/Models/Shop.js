595aeacf7754dfdb69dd1050911ca9c8d66a6f4b
Ehsan <unkmitm@gmail.com>
2025-08-08 20:32:38 +0330

    multer and path

--- backend/Models/Shop.js
+++ backend/Models/Shop.js
@@ -4,9 +4,9 @@ const shopSchema = mongoose.Schema({
   name: { type: String, required: [true, "Name is required"] },
   description: { type: String, required: [true, "Description is required"] },
   price: { type: Number, required: [true, "Price is required"] },
-  image: { type: String, required: [true, "Image is required"] },
-  colors: { type: [String] , default : [] },
-  category : { type: String, required: [true, "Category is required"] }
+  image: { type: String },
+  colors: { type: [String], default: [] },
+  categories: { type: [String], default: [] },
 });
 
 module.exports = mongoose.model("Shop", shopSchema);
--- backend/controllers/shop.js
+++ backend/controllers/shop.js
@@ -1,13 +1,27 @@
 const Shop = require("../Models/Shop");
 
 const shop = async (req, res) => {
-  const items = { ...req.body };
+  try {
+    const items = { ...req.body };
 
-  const checkItem = await Shop.findOne({ name: items.name });
-  if (checkItem) {
-    return res.status(400).json({ msg: "Item already exists" });
+    const checkItem = await Shop.findOne({ name: items.name });
+    if (checkItem) {
+      return res.status(400).json({ msg: "Item already exists" });
+    }
+
+    const newItem = await Shop.create({
+      name: items.name,
+      description: items.description,
+      price: items.price,
+      image: items.image,
+      colors: items.colors,
+      categories: items.categories,
+    });
+    res.status(201).json({ msg: "Item added successfully", item: newItem });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ msg: "Server error" });
   }
-
 };
 
 module.exports = shop;
