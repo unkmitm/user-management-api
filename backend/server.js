48dc6e29eb3e582181ec29aa9b218b5fb7ea4077
Ehsan <unkmitm@gmail.com>
2025-08-08 03:40:14 +0330

    connected to DB

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+const database = require("./connection/connect");
+
 require("dotenv").config();
 const router = require("./routes/router");
 
@@ -11,6 +13,16 @@ app.use("/api/auth", router);
 app.use("/api/basket", router);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+const start = async () => {
+  try {
+    await database(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
